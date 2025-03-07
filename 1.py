f747b0a88a14d1be1f0012c0c8cac57bbe419339
vladr <vladreva57@gmail.com>
2025-03-07 17:35:21 +0500

    first

---
+++ 1.py
@@ -0,0 +1,13 @@
+import scipy
+
+mean = 249
+sigma = 10
+n = 100
+alpha = 0.9
+
+percentile = scipy.stats.norm.ppf(alpha / 2)
+
+l = mean + percentile * sigma / (n ** .5)
+r = mean - percentile * sigma / (n ** .5)
+
+print(f"[{l}, {r}]")
